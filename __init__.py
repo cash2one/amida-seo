c23b61cf8d6083c5f6e6372916956f512adc2f39
Trang <t-mai@amida.co.jp>
2016-07-21 15:01:17 +0900

    version 0.2

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-
+#
+# Mai Xuan Trang
+# Flask application
+#
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
